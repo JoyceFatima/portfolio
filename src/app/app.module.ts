0db1bb0836e0ccd6b415e4a8e48f62dba80a4bd3
JoyceFatima <guilherme.horta@flarconsulting.com.br>
2022-07-04 17:48:26 -0300

    style: fix header and app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppRoutingModule } from './app-routing.module';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
+
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -10,8 +17,9 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { ShareComponent } from './components/pages/share/share.component';
 import { ContactComponent } from './components/pages/contact/contact.component';
-import { AppRoutingModule } from './app-routing.module';
 import { BlogComponent } from './components/pages/blog/blog.component';
+
+registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
   declarations: [
@@ -29,8 +37,29 @@ import { BlogComponent } from './components/pages/blog/blog.component';
     RouterModule,
     AppRoutingModule,
     FontAwesomeModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+    },
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+// AOT compilation support  
+export function createTranslateLoader(http: HttpClient) {  
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');  
+}  
